3f5329cf99cc4bcb61d59df74a2c06726add9576
qwertyu <luke.z24680@gmail.com>
2025-04-21 17:30:32 -0400

    made rsft backspace for more homerow time

--- dev.nix
+++ dev.nix
@@ -41,37 +41,39 @@
 	config = ''
 	  (defsrc
 	    a    ;
-            s    l
-            d    k
-            f    j
-          )
-          
-          (defvar
-            tap-time 200
-            hold-time 200
-          )
-          
-          (defalias
-            a-mod (tap-hold $tap-time $hold-time a lmet )
-            sc-mod (tap-hold $tap-time $hold-time ; lmet )
-            s-mod (tap-hold $tap-time $hold-time s lctrl )
-            l-mod (tap-hold $tap-time $hold-time l lctrl )
-            d-mod (tap-hold $tap-time $hold-time d lalt )
-            k-mod (tap-hold $tap-time $hold-time k lalt )
-            f-mod (tap-hold $tap-time $hold-time f lsft )
-            j-mod (tap-hold $tap-time $hold-time j lsft )
-          )
-          
-          (deflayer homerow-mods
-	    @a-mod
-	    @sc-mod
-            @s-mod
-            @l-mod
-            @d-mod
-            @k-mod
-            @f-mod
-            @j-mod
-          )
+        s    l
+        d    k
+        f    j
+		rsft
+      )
+      
+      (defvar
+        tap-time 200
+        hold-time 200
+      )
+      
+      (defalias
+        a-mod (tap-hold $tap-time $hold-time a lmet )
+        sc-mod (tap-hold $tap-time $hold-time ; lmet )
+        s-mod (tap-hold $tap-time $hold-time s lctrl )
+        l-mod (tap-hold $tap-time $hold-time l lctrl )
+        d-mod (tap-hold $tap-time $hold-time d lalt )
+        k-mod (tap-hold $tap-time $hold-time k lalt )
+        f-mod (tap-hold $tap-time $hold-time f lsft )
+        j-mod (tap-hold $tap-time $hold-time j lsft )
+      )
+      
+      (deflayer homerow-mods
+        @a-mod
+        @sc-mod
+        @s-mod
+        @l-mod
+        @d-mod
+        @k-mod
+        @f-mod
+        @j-mod
+		bspc
+      )
 	'';
       };
       usbKeyboard = {
@@ -82,37 +84,39 @@
 	config = ''
 	  (defsrc
 	    a    ;
-            s    l
-            d    k
-            f    j
-          )
-          
-          (defvar
-            tap-time 200
-            hold-time 200
-          )
-          
-          (defalias
-            a-mod (tap-hold $tap-time $hold-time a lmet )
-            sc-mod (tap-hold $tap-time $hold-time ; lmet )
-            s-mod (tap-hold $tap-time $hold-time s lctrl )
-            l-mod (tap-hold $tap-time $hold-time l lctrl )
-            d-mod (tap-hold $tap-time $hold-time d lalt )
-            k-mod (tap-hold $tap-time $hold-time k lalt )
-            f-mod (tap-hold $tap-time $hold-time f lsft )
-            j-mod (tap-hold $tap-time $hold-time j lsft )
-          )
-          
-          (deflayer homerow-mods
-	    @a-mod
-	    @sc-mod
-            @s-mod
-            @l-mod
-            @d-mod
-            @k-mod
-            @f-mod
-            @j-mod
-          )
+        s    l
+        d    k
+        f    j
+		rsft
+      )
+      
+      (defvar
+        tap-time 200
+        hold-time 200
+      )
+      
+      (defalias
+        a-mod (tap-hold $tap-time $hold-time a lmet )
+        sc-mod (tap-hold $tap-time $hold-time ; lmet )
+        s-mod (tap-hold $tap-time $hold-time s lctrl )
+        l-mod (tap-hold $tap-time $hold-time l lctrl )
+        d-mod (tap-hold $tap-time $hold-time d lalt )
+        k-mod (tap-hold $tap-time $hold-time k lalt )
+        f-mod (tap-hold $tap-time $hold-time f lsft )
+        j-mod (tap-hold $tap-time $hold-time j lsft )
+      )
+      
+      (deflayer homerow-mods
+        @a-mod
+        @sc-mod
+        @s-mod
+        @l-mod
+        @d-mod
+        @k-mod
+        @f-mod
+        @j-mod
+	    bspc
+      )
 	'';
       };
     };
