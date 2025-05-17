de09429e2f77e240347e38b08fd0c9b5ba984ca6
qwertyu <luke.z24680@gmail.com>
2025-05-16 22:55:26 -0400

    keyboard should be good now, also cleaned it up a little with indentation

--- dev.nix
+++ dev.nix
@@ -37,68 +37,79 @@
         devices = [
           "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
         ];
-      extraDefCfg = "process-unmapped-keys yes";
-      config = ''
-        (defsrc
-          grv       1    2    3    4    5    6    7    8    9    0    -    =    bspc
-          tab       q    w    e    r    t    y    u    i    o    p    [    ]    \
-          caps      a    s    d    f    g    h    j    k    l    ;    '         ret
-          lsft      z    x    c    v    b    n    m    ,    .    /    rsft
-          lctl lmet lalt           spc                 ralt rmet rctl
-        )
-        
-        (defvar
-          tap-time 200
-          hold-time 200
-        )
-        
-        (defalias
-          grl (tap-hold 200 200 grv (layer-toggle layers))
-          gal (layer-switch gallium)
-          qwr (layer-switch qwerty)
-          col (layer-switch colemak)
-          lm (tap-hold $tap-time $hold-time a lmet )
-          rm (tap-hold $tap-time $hold-time ; lmet )
-          lc (tap-hold $tap-time $hold-time s lctrl )
-          rc (tap-hold $tap-time $hold-time l lctrl )
-          la (tap-hold $tap-time $hold-time d lalt )
-          ra (tap-hold $tap-time $hold-time k lalt )
-          ls (tap-hold $tap-time $hold-time f lsft )
-          rs (tap-hold $tap-time $hold-time j lsft )
-        )
-        
-        (deflayer layers
-          _    @qwr @gal @col _    _    _    _    _    _    _    _    _    _
-          _    _    _    _    _    _    _    _    _    _    _    _    _    _
-          _    _    _    _    _    _    _    _    _    _    _    _    _
-          _    _    _    _    _    _    _    _    _    _    _    _
-          _    _    _              _              _    _    _
-        )
+        extraDefCfg = "process-unmapped-keys yes";
+        config = ''
+          (defsrc
+            grv       1    2    3    4    5    6    7    8    9    0    -    =    bspc
+            tab       q    w    e    r    t    y    u    i    o    p    [    ]    \
+            caps      a    s    d    f    g    h    j    k    l    ;    '         ret
+            lsft      z    x    c    v    b    n    m    ,    .    /    rsft
+            lctl lmet lalt           spc                 ralt rmet rctl
+          )
+          
+          (defvar
+            tap-time 200
+            hold-time 200
+          )
+          
+          (defalias
+            grl (tap-hold $tap-time $hold-time grv (layer-toggle layers))
+            gal (layer-switch gallium)
+            qwr (layer-switch qwerty)
+            col (layer-switch colemak)
+            ;; colemak
+            lmc (tap-hold $tap-time $hold-time a lmet )
+            rmc (tap-hold $tap-time $hold-time o lmet )
+            lcc (tap-hold $tap-time $hold-time r lctrl )
+            rcc (tap-hold $tap-time $hold-time i lctrl )
+            lac (tap-hold $tap-time $hold-time s lalt )
+            rac (tap-hold $tap-time $hold-time e lalt )
+            lsc (tap-hold $tap-time $hold-time t lsft )
+            rsc (tap-hold $tap-time $hold-time n lsft )
+            :: gallium
+            lmg (tap-hold $tap-time $hold-time n lmet )
+            rmg (tap-hold $tap-time $hold-time i lmet )
+            lcg (tap-hold $tap-time $hold-time r lctrl )
+            rcg (tap-hold $tap-time $hold-time e lctrl )
+            lag (tap-hold $tap-time $hold-time t lalt )
+            rag (tap-hold $tap-time $hold-time a lalt )
+            lsg (tap-hold $tap-time $hold-time s lsft )
+            rsg (tap-hold $tap-time $hold-time h lsft )
 
-        (deflayer qwerty
-          @grl      1    2    3    4    5    6    7    8    9    0    -    =    bspc
-          tab       q    w    e    r    t    y    u    i    o    p    [    ]    \
-          esc       @lm  @lc  @la  @ls  g    h    @rs  @ra  @rc  @rm  '         ret
-          lsft      z    x    c    v    b    n    m    ,    .    /    rsft
-          lctl lmet lalt           spc                 ralt rmet rctl
-        )
+          )
+          
+          (deflayer layers
+            _    @qwr @gal @col _    _    _    _    _    _    _    _    _    _
+            _    _    _    _    _    _    _    _    _    _    _    _    _    _
+            _    _    _    _    _    _    _    _    _    _    _    _    _
+            _    _    _    _    _    _    _    _    _    _    _    _
+            _    _    _              _              _    _    _
+          )
 
-        (deflayer gallium
-          @grl      1    2    3    4    5    6    7    8    9    0    -    =    bspc
-          tab       b    l    d    c    v    j    f    o    u    ,    [    ]    \
-          esc       @lm  r    t    s    g    y    h    a    e    i    /         ret
-          lsft      x    q    m    w    z    k    p    '    ;    .    rsft
-          lctl lmet lalt           spc                 ralt rmet rctl
-        )
+          (deflayer qwerty
+            @grl      1    2    3    4    5    6    7    8    9    0    -    =    bspc
+            tab       q    w    e    r    t    y    u    i    o    p    [    ]    \
+            esc       a    s    d    f    g    h    j    k    l    ;    '         ret
+            lsft      z    x    c    v    b    n    m    ,    .    /    rsft
+            lctl lmet lalt           spc                 ralt rmet rctl
+          )
 
-        (deflayer colemak
-          @grl      1    2    3    4    5    6    7    8    9    0    -    =    bspc
-          tab       q    w    f    p    g    j    l    u    y    ;    [    ]    \
-          esc       a    r    s    t    d    h    n    e    i    o    '         ret
-          lsft      z    x    c    v    b    k    m    ,    .    /    rsft
-          lctl lmet lalt           spc                 ralt rmet rctl
-        )
-      '';
+          (deflayer gallium
+            @grl      1    2    3    4    5    6    7    8    9    0    -    =    bspc
+            tab       b    l    d    c    v    j    f    o    u    ,    [    ]    \
+            esc       @lmg @lcg @lag @lsg g    y    @rsg @rag @rcg @rmg /         ret
+            lsft      x    q    m    w    z    k    p    '    ;    .    rsft
+            lctl lmet lalt           spc                 ralt rmet rctl
+          )
+
+          (deflayer colemak
+            @grl      1    2    3    4    5    6    7    8    9    0    -    =    bspc
+            tab       q    w    f    p    g    j    l    u    y    ;    [    ]    \
+            esc       @lmc @lcc @lac @lsc g    y    @rsc @rac @rcc @rmc '         ret
+            lsft      z    x    c    v    b    k    m    ,    .    /    rsft
+            lctl lmet lalt           spc                 ralt rmet rctl
+          )
+        '';
       };
       usbKeyboard = {
         devices = [
@@ -106,66 +117,77 @@
         ];
         extraDefCfg = "process-unmapped-keys yes";
         config = ''
-        (defsrc
-          grv       1    2    3    4    5    6    7    8    9    0    -    =    bspc
-          tab       q    w    e    r    t    y    u    i    o    p    [    ]    \
-          caps      a    s    d    f    g    h    j    k    l    ;    '         ret
-          lsft      z    x    c    v    b    n    m    ,    .    /    rsft
-          lctl lmet lalt           spc                 ralt rmet rctl
-        )
-        
-        (defvar
-          tap-time 200
-          hold-time 200
-        )
-        
-        (defalias
-          grl (tap-hold 200 200 grv (layer-toggle layers))
-          gal (layer-switch gallium)
-          qwr (layer-switch qwerty)
-          col (layer-switch colemak)
-          lm (tap-hold $tap-time $hold-time a lmet )
-          rm (tap-hold $tap-time $hold-time ; lmet )
-          lc (tap-hold $tap-time $hold-time s lctrl )
-          rc (tap-hold $tap-time $hold-time l lctrl )
-          la (tap-hold $tap-time $hold-time d lalt )
-          ra (tap-hold $tap-time $hold-time k lalt )
-          ls (tap-hold $tap-time $hold-time f lsft )
-          rs (tap-hold $tap-time $hold-time j lsft )
-        )
-        
-        (deflayer layers
-          _    @qwr @gal @col _    _    _    _    _    _    _    _    _    _
-          _    _    _    _    _    _    _    _    _    _    _    _    _    _
-          _    _    _    _    _    _    _    _    _    _    _    _    _
-          _    _    _    _    _    _    _    _    _    _    _    _
-          _    _    _              _              _    _    _
-        )
+          (defsrc
+            grv       1    2    3    4    5    6    7    8    9    0    -    =    bspc
+            tab       q    w    e    r    t    y    u    i    o    p    [    ]    \
+            caps      a    s    d    f    g    h    j    k    l    ;    '         ret
+            lsft      z    x    c    v    b    n    m    ,    .    /    rsft
+            lctl lmet lalt           spc                 ralt rmet rctl
+          )
+          
+          (defvar
+            tap-time 200
+            hold-time 200
+          )
+          
+          (defalias
+            grl (tap-hold $tap-time $hold-time grv (layer-toggle layers))
+            gal (layer-switch gallium)
+            qwr (layer-switch qwerty)
+            col (layer-switch colemak)
+            ;; colemak
+            lmc (tap-hold $tap-time $hold-time a lmet )
+            rmc (tap-hold $tap-time $hold-time o lmet )
+            lcc (tap-hold $tap-time $hold-time r lctrl )
+            rcc (tap-hold $tap-time $hold-time i lctrl )
+            lac (tap-hold $tap-time $hold-time s lalt )
+            rac (tap-hold $tap-time $hold-time e lalt )
+            lsc (tap-hold $tap-time $hold-time t lsft )
+            rsc (tap-hold $tap-time $hold-time n lsft )
+            :: gallium
+            lmg (tap-hold $tap-time $hold-time n lmet )
+            rmg (tap-hold $tap-time $hold-time i lmet )
+            lcg (tap-hold $tap-time $hold-time r lctrl )
+            rcg (tap-hold $tap-time $hold-time e lctrl )
+            lag (tap-hold $tap-time $hold-time t lalt )
+            rag (tap-hold $tap-time $hold-time a lalt )
+            lsg (tap-hold $tap-time $hold-time s lsft )
+            rsg (tap-hold $tap-time $hold-time h lsft )
 
-        (deflayer qwerty
-          @grl      1    2    3    4    5    6    7    8    9    0    -    =    bspc
-          tab       q    w    e    r    t    y    u    i    o    p    [    ]    \
-          esc       @lm  @lc  @la  @ls  g    h    @rs  @ra  @rc  @rm  '         ret
-          lsft      z    x    c    v    b    n    m    ,    .    /    rsft
-          lctl lmet lalt           spc                 ralt rmet rctl
-        )
+          )
+          
+          (deflayer layers
+            _    @qwr @gal @col _    _    _    _    _    _    _    _    _    _
+            _    _    _    _    _    _    _    _    _    _    _    _    _    _
+            _    _    _    _    _    _    _    _    _    _    _    _    _
+            _    _    _    _    _    _    _    _    _    _    _    _
+            _    _    _              _              _    _    _
+          )
 
-        (deflayer gallium
-          @grl      1    2    3    4    5    6    7    8    9    0    -    =    bspc
-          tab       b    l    d    c    v    j    f    o    u    ,    [    ]    \
-          esc       @lm  r    t    s    g    y    h    a    e    i    /         ret
-          lsft      x    q    m    w    z    k    p    '    ;    .    rsft
-          lctl lmet lalt           spc                 ralt rmet rctl
-        )
+          (deflayer qwerty
+            @grl      1    2    3    4    5    6    7    8    9    0    -    =    bspc
+            tab       q    w    e    r    t    y    u    i    o    p    [    ]    \
+            esc       a    s    d    f    g    h    j    k    l    ;    '         ret
+            lsft      z    x    c    v    b    n    m    ,    .    /    rsft
+            lctl lmet lalt           spc                 ralt rmet rctl
+          )
 
-        (deflayer colemak
-          @grl      1    2    3    4    5    6    7    8    9    0    -    =    bspc
-          tab       q    w    f    p    g    j    l    u    y    ;    [    ]    \
-          esc       a    r    s    t    d    h    n    e    i    o    '         ret
-          lsft      z    x    c    v    b    k    m    ,    .    /    rsft
-          lctl lmet lalt           spc                 ralt rmet rctl
-        )
-      '';
+          (deflayer gallium
+            @grl      1    2    3    4    5    6    7    8    9    0    -    =    bspc
+            tab       b    l    d    c    v    j    f    o    u    ,    [    ]    \
+            esc       @lmg @lcg @lag @lsg g    y    @rsg @rag @rcg @rmg /         ret
+            lsft      x    q    m    w    z    k    p    '    ;    .    rsft
+            lctl lmet lalt           spc                 ralt rmet rctl
+          )
+
+          (deflayer colemak
+            @grl      1    2    3    4    5    6    7    8    9    0    -    =    bspc
+            tab       q    w    f    p    g    j    l    u    y    ;    [    ]    \
+            esc       @lmc @lcc @lac @lsc g    y    @rsc @rac @rcc @rmc '         ret
+            lsft      z    x    c    v    b    k    m    ,    .    /    rsft
+            lctl lmet lalt           spc                 ralt rmet rctl
+          )
+        '';
       };
     };
   };
