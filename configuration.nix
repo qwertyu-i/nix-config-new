564853d38e98e782c7c94fd78374c76f038bd5bf
qwertyu <luke.z24680@gmail.com>
2025-05-16 22:40:47 -0400

    final touches

--- configuration.nix
+++ configuration.nix
@@ -36,14 +36,6 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "colemak";
-  };
-
-  console.keyMap = "colemak";
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.qwertyu = {
     isNormalUser = true;
