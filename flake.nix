f890cdc7d97675a00564aaab56d364741d60e11c
qwertyu <luke.z24680@gmail.com>
2026-08-18 18:43:08 -0400

    i think it builds idk i added ardour btw

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v1.0.0";
+      url = "github:nix-community/lanzaboote/v1.1.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
--- modules/software/ardour.nix
+++ modules/software/ardour.nix
@@ -4,5 +4,10 @@
   environment.systemPackages = with pkgs; [
     ardour
     neural-amp-modeler-lv2
+    carla
   ];
+
+  environment.variables = {
+    LV2_PATH = "/run/current-system/sw/lib/lv2";
+  };
 }
